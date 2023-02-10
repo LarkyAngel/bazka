4293562d9893a32a50a98d69ffd19f73a39c3a19
LarkyAngel <59319622+LarkyAngel@users.noreply.github.com>
2023-02-10 23:01:45 +0100

    final

---
+++ src/run.sh
@@ -0,0 +1,14 @@
+echo podaj username:
+read username
+echo podaj hasło:
+read password
+
+export PGPASSWORD="$password"
+
+FILE=/skijumping/Main.class
+if ! [[ -f "$FILE" ]]; then
+	psql -d postgres -U $username -f tabele.sql
+	javac --release 8 -cp ".:postgresql-42.5.3.jar" skijumping/*.java
+fi
+
+java -cp ".:postgresql-42.5.3.jar" skijumping.Main $username $password
